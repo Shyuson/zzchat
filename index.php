3f7d7afd67a9c703718d2ef70cccec4d772761d9
Shyuson <root@debian>
2016-11-11 16:12:42 -0500

    forget file

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<!on fait une commentaire la>
+<head>
+	<title>My very fist page</title>
+	<link rel="stylesheet" type="text/css" href="./bootstrap.min.css">
+</head>
+
+<body>
+		<div class="container">
+			<div class="row main">
+				<div class="panel-heading">
+	               <div class="panel-title text-center">
+	               		<h1 class="title">ZZchat</h1>
+	               		<hr />
+	               	</div>
+	            </div> 
+				<div class="main-login main-center">
+					<form class="form-horizontal" action="check.php" method="post" >
+						
+						<div class="form-group">
+							<label for="name" class="cols-sm-2 control-label">Login</label>
+							<div class="cols-sm-10">
+								<div class="input-group">
+									<input type="text" class="form-control" name="pseudo" id="name"  placeholder="Enter your Pseudo"/>
+								</div>
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="password" class="cols-sm-2 control-label">Password</label>
+							<div class="cols-sm-10">
+								<div class="input-group">
+									<input type="password" class="form-control" name="password" id="password"  placeholder="Enter your Password"/>
+								</div>
+							</div>
+						</div>
+						<div class="form-group ">
+							<!<button type="button" class="btn btn-primary btn-lg btn-block login-button"> 
+							<a href="inscription.php">Register</a> 
+							<!</button>
+
+
+						</div>
+						<div class="login-register">
+				            <input type="submit" value="Login">
+				         </div>
+					</form>
+				</div>
+			</div>
+		</div>
+
+		<script type="text/javascript" src="assets/js/bootstrap.js"></script>
+	</body>
+</html>
